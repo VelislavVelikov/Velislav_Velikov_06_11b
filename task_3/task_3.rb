c77ffe63f8d2f11912a8d16c27d2c09ba5debcf1
Velislav Velikov <velislavvelikov2@gmail.com>
2017-09-05 12:58:02 +0300

    FINALLY task_3.rb works, I changed a little the 'csv' file

--- task_3/task_3.rb
+++ task_3/task_3.rb
@@ -1,19 +1,34 @@
 require 'json'
-require 'math'
+#require 'Math'
 
-json = JSON.parse(ARGV[0].to_s)
+file = File.read(ARGV[0].to_s)
+json = JSON.parse(file)
 
-a = json['a'].to_i
-b = json['b'].to_i
-c = json['c'].to_i
+a = json['a'].to_f.round(2)
+b = json['b'].to_f.round(2)
+c = json['c'].to_f.round(2)
 
 d = b*b-4*a*c
 
 if(d>0) then
-	puts 'x1='+((-b-Math.sqrt(d))/(2*a)).to_s
-	puts 'x2='+((-b+Math.sqrt(d))/(2*a)).to_s
-else if(d == 0) then
-	puts 'x1 and x2='+((-b/(2*a)).to_s
-else
-	puts 'No resolve'
-)
+	puts "{\n"
+	puts '	"x1": "'+((-b-Math.sqrt(d))/(2*a)).to_s+'"'
+	puts '	"x2": "'+((-b+Math.sqrt(d))/(2*a)).to_s+'"'
+	puts '	"D": "'+ d.to_s+'"'
+	puts "}\n"
+
+end
+if(d == 0) then
+	puts "{\n"
+	puts '	"x1": "'+(-b/(2*a)).to_s+'",'
+	puts '	"x2": "'+(-b/(2*a)).to_s+'",'
+	puts '	"D": "'+ d.to_s+'"'
+	puts "}\n"
+end
+if(d<0) then
+	puts "{\n"
+	puts '	"x1": "NULL",'
+	puts '	"x2": "NULL",'
+	puts '	"D": "' + d.to_s + '"'
+	puts "}\n"
+end
